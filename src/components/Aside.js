cc94a9b6ac5ab5c43005c3ece6916fc9ba7b1e55
FOUDRE\Foudre II <laurent.digitick@gmail.com>
2022-03-23 16:41:54 +0100

    Build 2

---
+++ src/components/Aside.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Aside = () => {
+    return (
+        <div>
+            <h1>Aside</h1>
+        </div>
+    );
+};
+
+export default Aside;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Aside from '../components/Aside';
+import Header from '../components/Header';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <h1>Home</h1>
+            <Aside />
+        </div>
+    );
+};
+
+export default Home;
