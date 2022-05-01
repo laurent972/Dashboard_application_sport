677014f8e75b2a30f65ffa84205bdb9edaa17d9c
FOUDRE\Foudre II <laurent.digitick@gmail.com>
2022-05-01 11:34:41 +0200

    404 affichage

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <BrowserRouter>
         <Routes> 
           <Route path="rega_laurent_12_sport-see_220322/:id" element ={<Home />} />
-          <Route path="/*" element = { <Navigate replace to='rega_laurent_12_sport-see_220322/12' /> } />
+          {/* <Route path="/*" element = { <Navigate replace to='rega_laurent_12_sport-see_220322/12' /> } /> */}
           <Route path="/*" element ={<NotFound />}  />
         </Routes>
       </BrowserRouter>
